ccf8a58d1c642ad31e9c38bbbbd00a0e40e3684c
Enrique Mendoza <luis.e.1998@hotmail.com>
2022-04-23 23:15:52 -0700

    update UserService.test.js

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -4,9 +4,7 @@ describe('Test for UserView', () => {
     test('Return an error object when try to create a new user with an null payload', () => {
         const payload = null;
         const result = UserView.createUser(payload);
-
-
-
+        
         expect(result.error).toMatch(/payload no existe/);
     });
 
